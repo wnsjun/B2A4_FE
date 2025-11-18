215ce6e961ac2a57680e45c001700b964adccaad
wnsjun <shadd3338@gmail.com>
2025-11-18 06:14:30 +0000

    #76 [FIX] kakaomap 빌드에러 해결

--- src/pages/Hospitalmap.tsx
+++ src/pages/Hospitalmap.tsx
@@ -8,8 +8,6 @@ import hospitalImage from "../assets/hospitalmap/hospitalimage.png";
 import MyLocation from "../assets/hospitalmap/mylocation.png";
 import LocationPin from "../assets/hospitalmap/locationpin.png";
 import { debounce } from "lodash";
-import { useKakaoMaps } from "../hooks/useKakaoMaps";
-
 declare global {
   interface Window {
     kakao: any;
@@ -77,7 +75,6 @@ const HOSPITAL_DATA: Hospital[] = [
 
 const Hospitalmap = () => {
   const navigate = useNavigate();
-  const { isReady: kakaoReady } = useKakaoMaps();
   const [modalOpen, setModalOpen] = useState(true);
   const [selectedHospital, setSelectedHospital] = useState<Hospital | null>(null);
   const [favorites, setFavorites] = useState<Set<number>>(new Set());
